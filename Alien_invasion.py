2bec83d8cb476aa601da33d953b72a4a820c435c
Czekoladson <karrigan97@interia.pl>
2024-05-05 00:43:58 +0200

    Lekka refraktoryzacja

--- Alien_invasion.py
+++ Alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion():
     def __init__(self):
@@ -11,14 +12,21 @@ class AlienInvasion():
         self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption('Inwazja Kosmitów')
 
+        self.ship = Ship(self)
+
     def run_game(self):
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self.check_events()
+            self.update_screen()
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            self.screen.fill(self.settings.bg_color)
-            pygame.display.flip()
+    def update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
 
 if __name__ == '__main__':
     ai = AlienInvasion()
