0b40ca6ba4bad0ffbb0bb5966f86599cf28d53c7
Czekoladson <karrigan97@interia.pl>
2024-05-05 20:02:22 +0200

    Dodanie ruchu statku

--- Alien_invasion.py
+++ Alien_invasion.py
@@ -18,10 +18,27 @@ class AlienInvasion():
         while True:
             self.check_events()
             self.update_screen()
+            self.ship.update()
     def check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self.check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self.check_keyup_events(event)
+    def check_keydown_events(self,event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_ESCAPE:
+            sys.exit()
+    def check_keyup_events(self,event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def update_screen(self):
         self.screen.fill(self.settings.bg_color)
